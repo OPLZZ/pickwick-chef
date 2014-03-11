824f381e20c0edc53fef23aff00986223d034768
Ondřej Bartas <ondrej@bartas.cz>
2014-03-11 10:18:19 +0100

    [APPLICATIONS][APP] Enable to server APP on all urls

--- cookbooks/applications/attributes/app.rb
+++ cookbooks/applications/attributes/app.rb
@@ -2,5 +2,5 @@
 default.applications[:app][:name]           = 'pickwick-app'
 default.applications[:app][:repository]     = 'https://github.com/OPLZZ/pickwick-app.git'
 default.applications[:app][:revision]       = 'master'
-default.applications[:app][:url]            = 'app.damepraci.cz'
+default.applications[:app][:url]            = 'app.damepraci.cz _'
 default.applications[:app][:port]           = 80
