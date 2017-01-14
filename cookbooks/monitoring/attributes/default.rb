79800ab36d80d35cc82358601762a2277faaa9c6
Vojtech Hyza <vhyza@vhyza.eu>
2017-01-14 17:02:04 +0100

    [MONIT] Added monit email configuration

--- cookbooks/monitoring/attributes/default.rb
+++ cookbooks/monitoring/attributes/default.rb
@@ -4,3 +4,22 @@ default.monit[:start_delay]  = 60
 
 default.monit[:log]          = '/var/log/monit.log'
 default.monit[:pid]          = '/var/run/monit.pid'
+
+message = <<-EOS
+[damepraci] #{node.hostname} : $SERVICE $ACTION
+$DESCRIPTION
+--
+http://#{node.monit[:http_auth][:username]}:#{node.monit[:http_auth][:password]}@damepraci.cz:2812/
+EOS
+
+default.monit[:mail] = {
+  :hostname => "localhost",
+  :port     => 25,
+  :username => nil,
+  :password => nil,
+  :from     => "monit@$HOST",
+  :subject  => "[monit] [damepraci] $EVENT: $SERVICE at #{node.hostname}",
+  :message  => message,
+  :security => nil,  # 'SSLV2'|'SSLV3'|'TLSV1'
+  :timeout  => 30
+}
